41ef4a903672fea49c721917f7661bfbc2f96ac1
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-09-13 23:52:14 +0700

    Create chain validation

--- block.js
+++ block.js
@@ -24,6 +24,11 @@ class Block {
     return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 
+  static blockHash(block) {
+    const { timestamp, lastHash, data } = block;
+    return Block.hash(timestamp, lastHash, data);
+  }
+
   toString() {
     return `Block -
       Timestamp: ${this.timestamp}
