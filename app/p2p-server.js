58c027859d8d8981f39351e5eb8ef656b198d7fb
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-09-14 22:18:01 +0700

    Create connect peers function and implement p2p server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -12,6 +12,9 @@ class P2pServer {
   listen() {
     const server = new WebSocket.Server({ port: P2P_PORT });
     server.on('connection', socket => this.connectSocket(socket));
+
+    this.connectToPeers();
+
     console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
   }
 
@@ -19,4 +22,14 @@ class P2pServer {
     this.sockets.push(socket);
     console.log('Socket connected');
   }
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new WebSocket(peer);
+
+      socket.on('open', () => this.connectSocket(socket));
+    });
+  }
 }
+
+module.exports = P2pServer;
