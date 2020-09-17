3cdb1e995f7db25acae0eea006a10d7346f78f3c
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-09-17 22:04:16 +0700

    Create update transaction function

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -7,6 +7,23 @@ class Transaction {
     this.outputs = [];
   }
 
+  update(senderWallet, recipient, amount) {
+    const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey);
+
+    if (amount > senderOutput.amount) {
+      console.log(`Amount: ${amount} exceeds balance.`);
+      return;
+    }
+
+    senderOutput.amount = senderOutput.amount - amount;
+
+    this.outputs.push({ amount, address: recipient });
+
+    Transaction.signTransaction(this, senderWallet);
+
+    return this;
+  }
+
   static newTransaction(senderWallet, recipient, amount) {
     if (amount > senderWallet.balance) {
       console.log(`Amount: ${amount} exceeds balance.`);
